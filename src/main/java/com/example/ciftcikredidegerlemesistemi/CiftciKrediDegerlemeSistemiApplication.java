4e377c7a9991d65f1bed11276ac4456fa9768340
Erhan Taşkın <erhantaskin184@gmail.com>
2023-07-11 11:49:44 +0300

    first commit

---
+++ src/main/java/com/example/ciftcikredidegerlemesistemi/CiftciKrediDegerlemeSistemiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ciftcikredidegerlemesistemi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiftciKrediDegerlemeSistemiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiftciKrediDegerlemeSistemiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ciftcikredidegerlemesistemi/CiftciKrediDegerlemeSistemiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ciftcikredidegerlemesistemi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiftciKrediDegerlemeSistemiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
