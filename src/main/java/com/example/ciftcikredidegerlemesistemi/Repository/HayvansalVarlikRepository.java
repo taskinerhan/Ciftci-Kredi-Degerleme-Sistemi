736d67e4936884006ef209536236d52ff520696f
Erhan Taşkın <erhantaskin184@gmail.com>
2023-07-18 13:46:04 +0300

    HayvansalVarlikRepositoryde findByHayvanIrk fonksiyonu  eklendi.

--- src/main/java/com/example/ciftcikredidegerlemesistemi/Repository/HayvansalVarlikRepository.java
+++ src/main/java/com/example/ciftcikredidegerlemesistemi/Repository/HayvansalVarlikRepository.java
@@ -1,10 +1,14 @@
 package com.example.ciftcikredidegerlemesistemi.Repository;
-
 import com.example.ciftcikredidegerlemesistemi.Entity.Cinsiyet;
+import com.example.ciftcikredidegerlemesistemi.Entity.HayvanIrk;
 import com.example.ciftcikredidegerlemesistemi.Entity.HayvansalVarlikDeger;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface HayvansalVarlikRepository extends JpaRepository<HayvansalVarlikDeger,Long> {
-    public HayvansalVarlikDeger findByHayvanIrkIdAndCinsiyet(Long id, Cinsiyet cinsiyet);
+     HayvansalVarlikDeger findByHayvanIrkIdAndCinsiyet(Long id, Cinsiyet cinsiyet);
+     List<HayvansalVarlikDeger> findAll();
+     List<HayvansalVarlikDeger> findByHayvanIrk(HayvanIrk hayvanIrk);
 
 }
